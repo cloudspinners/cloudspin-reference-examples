0fd4ac0302e1efd5661ce789ee6f8151e7693c7b
kief <kmorris@thoughtworks.com>
2018-11-14 11:22:02 +0000

    Updated the environments tutorial to work with newer cloudspin gems, and to tidy up some things for the tutorial.

--- 02-environments/test/inspec/controls/vpc.rb
+++ 02-environments/test/inspec/controls/vpc.rb
@@ -2,14 +2,8 @@
 
 title 'vpc'
 
-stack_instance_id = attribute('stack_instance_id', description: 'Which stack to test')
+instance_identifier = attribute('instance_identifier', description: 'Which stack to test')
 
 describe aws_vpc_list do
-  its('name') { should include "vpc-#{stack_instance_id}" }
-end
-
-describe attribute('region', description: 'aws region') do
-  it 'region attribute should be available' do
-    expect(subject).not_to eq('')
-  end
+  its('name') { should include "vpc-#{instance_identifier}" }
 end
